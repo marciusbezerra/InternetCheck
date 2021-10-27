583d99d9165ea924f0272ee03092ed57c5e5b72d
Marcius Bezerra <marciusbezerra@outlook.com>
2021-10-26 23:54:40 -0300

    inicio

---
+++ FormMain.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1611, 67);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Location = new System.Drawing.Point(1397, 18);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(158, 29);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Iniciar testes";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 67);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1611, 555);
+            this.panel2.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.Black;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1611, 555);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Hora";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Status";
+            this.columnHeader2.Width = 350;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Resultado";
+            this.columnHeader3.Width = 700;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(494, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "INICIADO EM 00:00:00 | ERROS ENCONTRADO 1000";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1611, 622);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormMain : Form
+    {
+        private Timer Interval;
+        DateTime timeStart;
+        int ErrorCount;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            Interval = new Timer();
+            Interval.Elapsed += new ElapsedEventHandler(async (o, e) =>
+            {
+                HttpClient httpClient = new HttpClient();
+                try
+                {
+                    var content = await httpClient.GetStringAsync("https://www.google.com");
+                    if (!string.IsNullOrWhiteSpace(content))
+                    {
+                        AddListItem("Acessando google.com", "OK", false);
+                    }
+                    else throw new Exception("Nenhum conteúdo!");
+                }
+                catch (Exception ex)
+                {
+                    AddListItem("Acessando google.com", $"ERRO: {ex.Message}", true);
+                }
+            });
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (Interval.Enabled)
+            {
+                Interval.Enabled = false;
+                buttonStart.Text = "Iniciar testes";
+            }
+            else
+            {
+                Interval.Interval = 3000;
+                Interval.Enabled = true;
+                buttonStart.Text = "Parar testes";
+                if (timeStart == DateTime.MinValue) timeStart = DateTime.Now;
+                label1.Text = $"INICIADO EM {timeStart:HH:mm:ss} | ERROS ENCONTRADO {ErrorCount}";
+            }
+        }
+
+        public void AddListItem(string status, string result, bool error)
+        {
+            if (!InvokeRequired)
+            {
+                string[] row = { DateTime.Now.ToString("HH:mm:ss"), status, result };
+                var listViewItem = new ListViewItem(row);
+                listView1.Items.Add(listViewItem);
+                listViewItem.EnsureVisible();
+                listViewItem.ForeColor = error ? Color.Red : Color.LightGreen;
+                if (error) ErrorCount++;
+                label1.Text = $"INICIADO EM {timeStart:HH:mm:ss} | ERROS ENCONTRADO {ErrorCount}";
+            }
+            else
+            {
+                Invoke(new Action<string, string, bool>(AddListItem), status, result, error);
+            }
+        }
+    }
+}
